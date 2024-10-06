2aba1a7d1653b4d6252a556d1c6e62501f9782b9
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2024-10-06 13:59:22 +0300

    Feature/communication (#17)
    * Catalog API mini microservice crud almost done
    
    * MicroAPI service crud operations done.
    
    * Save pipelines
    
    * Microservice done
    
    * Remove .idea from repository
    
    * BasketApi microservice created and almost finished.
    
    * redis cache integration almost done
    
    * redis cache integration to basket done
    
    * Discount.grpc project created and written.
    
    * Composo file updated
    
    * Finished Ordering Domain Layer
    
    * Domain Driven Design(DDD)
    
    * Order API queries added.
    
    * Endpoints created.
    
    * Rabbitmq added PART 1
    
    * Rabbitmq added and consume actions created.
    
    ---------
    
    Co-authored-by: bugra <@gmail.com>

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -22,15 +22,12 @@ public class StoreBasketCommandHandler
 {
     private readonly IBasketRepository _basketRepository;
     private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
-    private readonly ILogger<StoreBasketCommandHandler> _logger;
 
     public StoreBasketCommandHandler(IBasketRepository basketRepository,
-        DiscountProtoService.DiscountProtoServiceClient discountProtoService,
-        ILogger<StoreBasketCommandHandler> logger)
+        DiscountProtoService.DiscountProtoServiceClient discountProtoService)
     {
         _basketRepository = basketRepository;
         _discountProtoService = discountProtoService;
-        _logger = logger;
     }
 
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
@@ -45,14 +42,11 @@ public class StoreBasketCommandHandler
 
     private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("*** Deducting discount inside ***");
         // communicate with discount.grpc
         foreach (var item in cart.Items)
         {
-            _logger.LogInformation("--- Processing discount item ---");
             var coupon = await _discountProtoService.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken:cancellationToken);
             item.Price -= coupon.Amount;
         }
-        _logger.LogInformation("*** Deducting discount out ***");
     }
 }
